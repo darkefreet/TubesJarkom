c891237533939e262f0e1c5899e556e2c8a2fccf
Wilhelmus <wilhelm_andrean_22@yahoo.com>
2015-11-25 14:34:46 +0700

    Inisialisasi

---
+++ JarkomTCP/src/jarkomtcp/TCPServer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jarkomtcp;
+
+import java.io.*; 
+import java.net.*;
+
+/**
+ *
+ * @author Wilhelm
+ */
+public class TCPServer {
+    
+}
