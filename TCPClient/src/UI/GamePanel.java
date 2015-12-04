05d918537dd3fb4caa642dc73687d3a0a9f6e3d5
deredet <lucky.cahyadi@yahoo.com>
2015-12-04 16:25:47 +0700

    Merge origin/master
    Conflicts:
    	TCPClient/src/UI/Lobby.form
    	TCPClient/src/UI/Lobby.java
    	TCPClient/src/tcpclient/Client.java

--- TCPClient/src/UI/GamePanel.java
+++ TCPClient/src/UI/GamePanel.java
@@ -62,9 +62,11 @@ public class GamePanel extends JPanel implements MouseListener {
     
     @Override
     public void mouseClicked(MouseEvent me) {
-        if(Client.canMove){
-            ((JLabel)me.getSource()).setText(Integer.toString(Client.user_id));
-            Point p = (Point) (((JLabel)me.getSource()).getClientProperty("Location"));
+        JLabel a = ((JLabel)me.getSource());
+        if(Client.canMove ){
+            a.setText(Integer.toString(Client.user_id));
+            Point p = (Point) ((a.getClientProperty("Location")));
+            
             Client.input = "("+p.x+","+p.y+")";
             Client.button_pressed = true;     
             
